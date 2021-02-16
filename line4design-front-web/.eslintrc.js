f7e72ae12731f82ca597d3a101e180bf0b64d4d3
im-not-developer <dev.luyoes20@gmail.com>
2021-02-16 22:35:19 +0900

    init

---
+++ line4design-front-web/.eslintrc.js
@@ -0,0 +1,80 @@
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  rules: {
+    'max-len': [
+      'error',
+      100,
+      2,
+      {
+        ignoreUrls: true,
+        ignoreComments: false,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+    'no-unused-vars': [
+      'warn',
+      {varsIgnorePattern: '^_', argsIgnorePattern: '^_'},
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {varsIgnorePattern: '^_', argsIgnorePattern: '^_'},
+    ],
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'always',
+        pathGroups: [
+          {
+            pattern: '~/**',
+            group: 'parent',
+            position: 'before',
+          },
+        ],
+      },
+    ],
+    'import/newline-after-import': ['warn'],
+    'react/jsx-no-undef': ['error', {allowGlobals: true}],
+    'import/no-unresolved': [
+      'error',
+      {
+        ignore: ['@env'],
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      'babel-module': {},
+      typescript: {
+        alwaysTryTypes: true,
+      },
+      node: {
+        extensions: [
+          '.android.js',
+          '.ios.js',
+          '.native.js',
+          '.web.js',
+          '.js',
+          '.android.ts',
+          '.android.tsx',
+          '.ios.ts',
+          '.ios.tsx',
+          '.native.ts',
+          '.native.tsx',
+          '.web.ts',
+          '.web.tsx',
+          '.ts',
+          '.tsx',
+        ],
+      },
+    },
+  },
+};
---
+++ line4design-front-web/src/Line4Design.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import MainPage from './pages/MainPage';
+
+interface Props {
+  
+}
+
+const Line4Design = (props: Props) => {
+  return (
+    <div>
+      <MainPage />
+    </div>
+  )
+}
+
+export default Line4Design
