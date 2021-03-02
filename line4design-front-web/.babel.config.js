39d6d36ab92c4d942ec0def88e4a58d0569a8196
im_not_developer <im.not.developerrr@gmail.com>
2021-03-02 23:35:31 +0900

    Update .babel.config.js, .babelrc.js, and 11 more files...

--- line4design-front-web/.babel.config.js
+++ line4design-front-web/.babel.config.js
@@ -1,27 +1,27 @@
+/** @format */
 const process = require('process');
+
 const COMMON_PLUGINS = [
   'babel-plugin-styled-components',
   '@babel/plugin-syntax-dynamic-import',
   '@babel/plugin-proposal-nullish-coalescing-operator',
-  '@babel/plugin-proposal-optional-chaining',
+  '@babel/plugin-proposal-optional-chaining'
 ];
 const config = {
   plugins: [
-    [ 'module-resolver',
-      {
-        // alias: {
-        // {
-          rootPathPrefix: '~',
-          rootPathSuffix: 'src'
-          // '~': './src',
-        },
-      // },
-    ],
-    ...COMMON_PLUGINS,
-  ],
+    // [
+    //   'module-resolver',
+    //   {
+    //     alias: {
+    //       '~': './src'
+    //     }
+    //   }
+    // ],
+    ...COMMON_PLUGINS
+  ]
 };
 module.exports = () => {
   // if (process.env.PLATFORM === 'web') {
-    return config;
+  return config;
   // }
 };
--- line4design-front-web/.eslintrc.js
+++ line4design-front-web/.eslintrc.js
@@ -1,24 +1,25 @@
+/** @format */
+
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   extends: [
     // "plugin:prettier/recommended",
-
-    "plugin:react/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:css-modules/recommended",
-    "prettier",
-    "prettier/react", 
-    "prettier/@typescript-eslint",
-    "plugin:@typescript-eslint/recommended",
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:css-modules/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+    'plugin:@typescript-eslint/recommended'
   ],
-  plugins: ["react", "@typescript-eslint", "prettier",  "css-modules",],
+  plugins: ['react', '@typescript-eslint', 'prettier', 'css-modules'],
 
   parserOptions: {
     ecmaVersion: 6,
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     }
   },
   rules: {
@@ -27,15 +28,15 @@ module.exports = {
     // "postcss-modules/no-unused-class": "warn",
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
-    "css-modules/no-unused-class": [2, { "camelCase": true }],
-    "css-modules/no-undef-class": [2, { "camelCase": true }],
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-   
-    "max-len": [
-      "error",
+    'css-modules/no-unused-class': [2, { camelCase: true }],
+    'css-modules/no-undef-class': [2, { camelCase: true }],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+
+    'max-len': [
+      'error',
       100,
       2,
       {
@@ -46,48 +47,50 @@ module.exports = {
         ignoreTemplateLiterals: true
       }
     ],
-    "no-unused-vars": [
-      "warn",
-      { varsIgnorePattern: "^_", argsIgnorePattern: "^_" }
+    'no-unused-vars': [
+      'warn',
+      { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }
     ],
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      { varsIgnorePattern: "^_", argsIgnorePattern: "^_" }
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }
     ],
-    "import/order": [
-      "warn",
+    'import/order': [
+      'warn',
       {
-        "newlines-between": "always",
+        'newlines-between': 'always',
         pathGroups: [
           {
-            pattern: "~/**",
-            group: "parent",
-            position: "before"
+            pattern: '~/**',
+            group: 'parent',
+            position: 'before'
           }
         ]
       }
     ],
-    "import/newline-after-import": ["warn"],
-    "react/jsx-no-undef": ["error", { allowGlobals: true }],
-    "import/no-unresolved": [
-      "error",
-      {
-        ignore: ["@env"]
-      }
-    ]
+    'import/newline-after-import': ['warn'],
+    'react/jsx-no-undef': ['error', { allowGlobals: true }],
+    'import/no-unresolved': 'off'
+    // [
+    //   'error',
+    //   {
+    //     ignore: ['@env']
+    //   }
+    // ]
   },
-  ignorePatterns: ["*.config.js"],
+  ignorePatterns: ['*.config.js'],
   settings: {
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"]
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
     },
-    "import/resolver": {
-      "babel-module": {},
+    'import/resolver': {
+      'babel-module': {},
       typescript: {
         alwaysTryTypes: true
       },
       node: {
-        extensions: [".ts", ".tsx"]
+        // moduleDirectory: ['node_modules', 'src'],
+        extensions: ['.ts', '.tsx']
       }
     }
   }
