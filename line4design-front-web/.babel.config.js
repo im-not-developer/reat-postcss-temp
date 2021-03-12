d50bfe075f6e4bd564cbe02d47d52ea336de3475
im-not-developer <dev.luyoes20@gmail.com>
2021-03-12 23:33:08 +0900

    Update .babel.config.js, config-overrides.js, and 2 more files...

--- line4design-front-web/.babel.config.js
+++ line4design-front-web/.babel.config.js
@@ -12,9 +12,9 @@ const config = {
     [
       'module-resolver',
       {
-        root: ['./'],
+        root: ['.'],
         alias: {
-          '~/*': './src'
+          '~': './src'
         }
       }
     ],
