a55a7959ef9c9ffa3d87c4c401d58ca0c9f5ccef
im-not-developer <dev.luyoes20@gmail.com>
2021-03-15 01:31:13 +0900

    all change

---
+++ line4design-fe/src/index.tsx
@@ -0,0 +1,20 @@
+/** @format */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import AppPresenter from './AppPresenter';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppPresenter />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ line4design-fe/src/pages/HomePage.tsx
@@ -0,0 +1,16 @@
+/** @format */
+
+import React from 'react';
+
+import HelloComponent from '../components/HelloComponent';
+
+const HomePage: React.FC = () => (
+  <div>
+    <div>Mainpage Component!</div>
+    <div>
+      <HelloComponent />
+    </div>
+  </div>
+);
+
+export default HomePage;
