c2521e0a62f80fae1585135672a2b972f80a460f
im_not_developer <im.not.developerrr@gmail.com>
2021-03-16 00:52:58 +0900

    Update config-overrides.js, package-lock.json, and 12 more files...

--- line4design-fe/config-overrides.js
+++ line4design-fe/config-overrides.js
@@ -1,7 +1,7 @@
 /** @format */
-
-const path = require('path');
-
+const postcssNested = require('postcss-nested');
+const postcssNesting = require('postcss-nesting');
+const reactAppRewirePostcss = require('react-app-rewire-postcss');
 const {
   useBabelRc,
   addWebpackAlias,
@@ -10,20 +10,24 @@ const {
 } = require('customize-cra');
 const rewireReactHotLoader = require('react-app-rewire-hot-loader-for-customize-cra');
 
-module.exports = override(
-  useBabelRc(),
-  rewireReactHotLoader(),
-  addWebpackAlias({
-    // 'react-dom': '@hot-loader/react-dom',
-    // 'react-dom$': 'react-dom/profiling',
-    // 'scheduler/tracing': 'scheduler/tracing-profiling',
-    '~': path.resolve(__dirname, './src')
-  }),
-  addBundleVisualizer(
-    {
-      analyzerMode: 'static',
-      reportFilename: 'report.html'
-    },
-    true
-  )
-);
+module.exports = config =>
+  override(
+    useBabelRc(),
+    rewireReactHotLoader(),
+    addWebpackAlias({
+      // 'react-dom': '@hot-loader/react-dom',
+      // 'react-dom$': 'react-dom/profiling',
+      // 'scheduler/tracing': 'scheduler/tracing-profiling',
+      '~': require('path').resolve(__dirname, './src')
+    }),
+    addBundleVisualizer(
+      {
+        analyzerMode: 'static',
+        reportFilename: 'report.html'
+      },
+      true
+    ),
+    reactAppRewirePostcss(config, {
+      plugins: () => [postcssNesting(), postcssNested()]
+    })
+  );
