fb7afc9b445c0b6ddf73e3228c4e8b04579b2b0f
im-not-developer <dev.luyoes20@gmail.com>
2021-03-19 00:45:53 +0900

    product section card dummy_data add

--- line4design-fe/src/components/ProductCard.tsx
+++ line4design-fe/src/components/ProductCard.tsx
@@ -1,11 +1,43 @@
 /** @format */
 
 import React from 'react';
+import { productListTypes } from '~/utils/product_list';
 
-interface Props {}
+interface productDefaultCardPropsType {
+  info?: productListTypes;
+}
 
-const ProductCard = (props: Props) => {
-  return <div>dd</div>;
+const productDefaultCard: productDefaultCardPropsType = {
+  info: {
+    id: 0,
+    category: '',
+    name: '',
+    price: 0,
+    discount_price: 0,
+    discount_percent: 0,
+    discount_on: false,
+    img_link: '',
+  },
+};
+
+const ProductCard = (props = productDefaultCard) => {
+  const { info } = props;
+  console.log(props);
+  return (
+    <div>
+      <div>
+        <div>
+          <div style={{ width: '100px' }}>
+            <img src={require(`~/assets/imgs/sample_img.jpeg`).default} />
+          </div>
+        </div>
+        <div>
+          down
+          <div>info</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
