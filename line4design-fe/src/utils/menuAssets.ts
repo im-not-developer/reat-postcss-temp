ba462bcecd96480001c51017c97cd31ddb71122c
im-not-developer <dev.luyoes20@gmail.com>
2021-03-24 01:31:51 +0900

    back

--- line4design-fe/src/utils/menuAssets.ts
+++ line4design-fe/src/utils/menuAssets.ts
@@ -3,50 +3,89 @@
 export interface sideMenuListTypes {
   id?: number;
   name?: string;
+  subMenu?: string;
   onMenu?: boolean;
 }
-export interface SELECT_MAP_TYPES {
-  [sideMenuList: string]: sideMenuListTypes[];
-}
-
-const sideMenuList: sideMenuListTypes[] = [
-  {
-    id: 0,
-    name: '전체',
-    onMenu: false,
-  },
-  {
-    id: 1,
-    name: 'CLOSTING',
-    onMenu: false,
-  },
-  {
-    id: 2,
-    name: 'ACC',
-    onMenu: false,
-  },
-  {
-    id: 3,
-    name: 'SKILL',
-    onMenu: false,
-  },
-];
 
 export interface allSubMenuTypes {
   id?: number;
   name?: string;
   onSubMenu?: boolean;
 }
+
+export interface SELECT_MAP_TYPES {
+  [k1: string]: sideMenuListTypes[];
+  [k2: string]: allSubMenuTypes[];
+}
+
+const sideMenuList: sideMenuListTypes[] = [
+  {
+    id: 0,
+    name: '전체',
+    subMenu: 'allSubMenu',
+    onMenu: false,
+  },
+  {
+    id: 1,
+    name: 'CLOSTING',
+    subMenu: 'allSubMenu',
+    onMenu: false,
+  },
+  {
+    id: 2,
+    name: 'ACC',
+    subMenu: 'allSubMenu',
+    onMenu: false,
+  },
+  {
+    id: 3,
+    name: 'SKILL',
+    subMenu: 'allSubMenu',
+    onMenu: false,
+  },
+];
+
 const allSubMenu: allSubMenuTypes[] = [
   {
     id: 0,
     name: 'side_menu_bar.all',
     onSubMenu: false,
   },
+  {
+    id: 1,
+    name: 'OUTER',
+    onSubMenu: false,
+  },
+  {
+    id: 2,
+    name: 'DRESSES',
+    onSubMenu: false,
+  },
+  {
+    id: 3,
+    name: 'SHIRTS / BLOUSES',
+    onSubMenu: false,
+  },
+  {
+    id: 4,
+    name: 'SWEATER / SWEATS',
+    onSubMenu: false,
+  },
+  {
+    id: 5,
+    name: 'T-SHIRTS',
+    onSubMenu: false,
+  },
+  {
+    id: 6,
+    name: 'PANTS / SKIRTS',
+    onSubMenu: false,
+  },
 ];
 
 const SELECT_MAP: SELECT_MAP_TYPES = {
   sideMenuList: sideMenuList,
+  allSubMenu: allSubMenu,
 };
 
 export const getSelectMenu = (key: string) => {
