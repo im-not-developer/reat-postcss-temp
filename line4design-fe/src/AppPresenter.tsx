18b58e3209ca7f82edc9555d606dc97c4a6151e5
im-not-developer <dev.luyoes20@gmail.com>
2021-04-10 02:42:56 +0900

    Update .eslintrc.js, .prettierrc.js, and 6 more files...

--- line4design-fe/src/AppPresenter.tsx
+++ line4design-fe/src/AppPresenter.tsx
@@ -2,9 +2,11 @@
 
 import React from 'react';
 import { SWRConfig } from 'swr';
+
 import Line4Design from './Line4Design';
 import LocaleProvider from './providers/LocaleProvider';
 import GlobalStyle from './styles/GlobalStyles';
+
 
 const AppPresenter: React.FC = () => {
   return (
--- line4design-fe/src/components/commons/Footer.tsx
+++ line4design-fe/src/components/commons/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = (props: Props) => {
             <div>라인포 디자인 / 서수이 / 서울시 용산구 신흥로</div>
             <div>개인정보관리 책임자 : 서수이</div>
             <div>대표메일주소: xxxxxx@xxxxx.com</div>
-            <div>
+      <div>
               통신판매신고: 서울 용산 - xxxxxx / 사업자등록번호{' '}
               <span>
                 <a href="#">사업자정보확인 x</a>
