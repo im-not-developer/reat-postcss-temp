e224d612824309c9999740a750a1246d3f00f895
im_not_developer <im.not.developerrr@gmail.com>
2021-05-03 23:52:32 +0900

    Update index.css, ClockIcon.tsx, and 7 more files...

--- line4design-fe/src/components/common/Footer.tsx
+++ line4design-fe/src/components/common/Footer.tsx
@@ -1,10 +1,46 @@
 import React, {FC} from 'react';
 
+import {
+  ClockIcon,
+  LocationMarkerIcon,
+  PhoneIcon,
+  LineMailIcon,
+} from '~/assets/imgs/components';
+
+const FooterBar: FC = () => {
+  return (
+    <div className="bottom_bar">
+      <div className="bottom_bar__container">
+        <div> Copyright © 2021 all rights reserved </div>
+      </div>
+    </div>
+  );
+};
 const Footer: FC = () => {
   return (
-    <div className="footer">
-      <div className="footer__container">FOOTER</div>
-    </div>
+    <>
+      <div className="footer">
+        <div className="footer__container">
+          <div className="footer__container__header">Contact</div>
+          <div className="footer__container__body">
+            <div>
+              <LineMailIcon /> <div>line4design@gmail.com</div>
+            </div>
+            <div>
+              <LocationMarkerIcon /> <div>Seoul, R.Korea</div>
+            </div>
+            <div>
+              <PhoneIcon />
+              <div>02.0202.0202</div>
+            </div>
+            <div>
+              <ClockIcon /> <div>am 10:00 ~ pm 06:00</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <FooterBar />
+    </>
   );
 };
 
